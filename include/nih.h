401f8bbe46d04cedb6a8be9d5621ecfa6b2cb16e
Michael Schwarz <michi.schwarz@gmail.com>
2009-04-13 23:32:46 +0200

    + As the endian.h header file is defines different macros on every platform, replacements are now conditionally put in place for what is missing.

---
+++ include/nih.h
@@ -0,0 +1,131 @@
+/*	Oscar, a hardware abstraction framework for the LeanXcam and IndXcam.
+	Copyright (C) 2008 Supercomputing Systems AG
+	
+	This library is free software; you can redistribute it and/or modify it
+	under the terms of the GNU Lesser General Public License as published by
+	the Free Software Foundation; either version 2.1 of the License, or (at
+	your option) any later version.
+	
+	This library is distributed in the hope that it will be useful, but
+	WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+	General Public License for more details.
+	
+	You should have received a copy of the GNU Lesser General Public License
+	along with this library; if not, write to the Free Software Foundation,
+	Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+/*! @file nih.h
+ * @brief NIH - "Not invented here."
+ * 
+ * Header file to support applications using the NIH design pattern.
+ */
+
+#ifndef OSCAR_INCLUDE_NIH_H_
+#define OSCAR_INCLUDE_NIH_H_
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <endian.h>
+
+/* This saves us two characters ... */
+typedef int8_t int8;
+typedef uint8_t uint8;
+typedef int16_t int16;
+typedef uint16_t uint16;
+typedef int32_t int32;
+typedef uint32_t uint32;
+typedef int64_t int64;
+typedef uint64_t uint64;
+typedef int OSC_ERR;
+typedef bool BOOL;
+
+#define TRUE true
+#define FALSE false
+
+/* Sadly, the GNU C Library does not define any of {le,be}{16,32,64}{enc,dec}, so we're using our own functions here. The naming schema has been adopted from the FreeBSD headers (http://www.digipedia.pl/man/bswap32.9.html). */
+#ifndef bswap16
+#  define bswap16(v) ((uint16_t) ((uint16_t) (v) << 8 | (uint16_t) (v) >> 8))
+#  define bswap32(v) ((uint32_t) ((uint32_t) bswap16(v) << 16 | (uint32_t) bswap16((v) >> 16)))
+#  define bswap64(v) ((uint64_t) ((uint64_t) bswap32(v) << 32 | (uint64_t) bswap32((v) >> 32)))
+#endif
+
+#ifndef htobe16
+#  if __BYTE_ORDER == __BIG_ENDIAN
+#    define htobe16
+#    define htole16 bswap16
+#    define be16toh
+#    define le16toh bswap16
+     
+#    define htobe32
+#    define htole32 bswap32
+#    define be32toh
+#    define le32toh bswap32
+     
+#    define htobe64
+#    define htole64 bswap64
+#    define be64toh
+#    define le64toh bswap64
+#  elif __BYTE_ORDER == __LITTLE_ENDIAN
+#    define htobe16 bswap16
+#    define htole16
+#    define be16toh bswap16
+#    define le16toh
+     
+#    define htobe32 bswap32
+#    define htole32
+#    define be32toh bswap32
+#    define le32toh
+     
+#    define htobe64 bswap64
+#    define htole64
+#    define be64toh bswap64
+#    define le64toh
+#  else
+#    error "__BYTE_ORDER is neither __BIG_ENDIAN nor __LITTLE_ENDIAN."
+#  endif
+#endif
+
+#ifndef be16dec
+#  define be16dec(p) ((uint16_t) ((uint16_t) *((uint8_t *) (p)) << 8 | (uint16_t) *((uint8_t *) (p) + 1)))
+#  define be32dec(p) ((uint32_t) ((uint32_t) be16dec(p) << 16 | (uint32_t) be16dec((uint16_t *) (p) + 1)))
+#  define be64dec(p) ((uint64_t) ((uint64_t) be32dec(p) << 32 | (uint64_t) be32dec((uint32_t *) (p) + 1)))
+   
+#  define be16enc(p, v) { *((uint8_t *) (p)) = (uint8_t) ((v) >> 8); *((uint8_t *) (p) + 1) = (uint8_t) (v); }
+#  define be32enc(p, v) { be16enc(p, (v) >> 16); be16enc((uint16_t *) (p) + 1, v); }
+#  define be64enc(p, v) { be32enc(p, (v) >> 32); be32enc((uint32_t *) (p) + 1, v); }
+   
+#  define le16dec(p) bswap16(be16dec(p))
+#  define le32dec(p) bswap32(be32dec(p))
+#  define le64dec(p) bswap64(be64dec(p))
+   
+#  define le16enc(p, v) be16enc(p, bswap16(v))
+#  define le32enc(p, v) be32enc(p, bswap32(v))
+#  define le64enc(p, v) be64enc(p, bswap64(v))
+#endif
+
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+/*! @brief Endianness of this machine. Intel uses LITTLE_ENDIAN */
+#define CPU_LITTLE_ENDIAN
+#endif
+
+/* ... and these are written in ergonomical ALL_CAPS; AWESMOE!!!11!1!! */
+/*! @brief Macro to swap the endianness of a 16 bit number */
+#define ENDIAN_SWAP_16 bswap16
+/*! @brief Macro to swap the endianness of a 32 bit number */
+#define ENDIAN_SWAP_32 bswap32
+/*! @brief Macro to swap the endianness of a 64 bit number */
+#define ENDIAN_SWAP_64 bswap64
+
+/*! @brief Write a 16 bit value to a non-aligned address. */
+#define ST_INT16 le16enc
+/*! @brief Write a 32 bit value to a non-aligned address. */
+#define ST_INT32 le32enc
+
+/*! @brief Load a 16 bit value from a non-aligned address. */
+#define LD_INT16 le16dec
+/*! @brief Load a 32 bit value from a non-aligned address. */
+#define LD_INT32 le32dec
+
+#endif /* OSCAR_INCLUDE_NIH_H_ */
--- include/oscar.h
+++ include/oscar.h
@@ -22,30 +22,15 @@
  * Must be included by the application.
  */
 
-#ifndef OSCAR_MAIN_HEADER_FILE_WHICH_MAY_BE_INCLUDED_FROM_INSIDE_A_MODULE_H_
-#define OSCAR_MAIN_HEADER_FILE_WHICH_MAY_BE_INCLUDED_FROM_INSIDE_A_MODULE_H_
+#ifndef OSCAR_INCLUDE_OSCAR_H_
+#define OSCAR_INCLUDE_OSCAR_H_
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#include <stdbool.h>
-#include <stdint.h>
-
-/* This saves to characters ... */
-typedef int8_t int8;
-typedef uint8_t uint8;
-typedef int16_t int16;
-typedef uint16_t uint16;
-typedef int32_t int32;
-typedef uint32_t uint32;
-typedef int64_t int64;
-typedef uint64_t uint64;
-typedef int OSC_ERR;
-typedef bool BOOL;
-
-#define TRUE true
-#define FALSE false
+/* Support file for the NIH design pattern. */
+#include "nih.h"
 
 #if defined(OSC_HOST)
 /* Defined as stumps because it is needed in code shared by target and
@@ -63,55 +48,6 @@ typedef bool BOOL;
 #else
 #error "Neither OSC_HOST nor OSC_TARGET is defined as a macro."
 #endif
-
-/*! @brief Endianness of this machine. Intel uses LITTLE_ENDIAN */
-#define CPU_LITTLE_ENDIAN
-
-/*! @brief Macro to swap the endianness of a 16 bit number */
-#define ENDIAN_SWAP_16(x) ((x>>8) | (x<<8))
-
-/*! @brief Macro to swap the endianness of a 32 bit number */
-#define ENDIAN_SWAP_32(x) ( \
-	(x>>24) | \
-	((x<<8) & 0x00FF0000) | \
-	((x>>8) & 0x0000FF00) | \
-	(x<<24) \
-)
-
-/*! @brief Macro to swap the endianness of a 64 bit number */
-#define ENDIAN_SWAP_64(x) ( \
-	(x>>56) | \
-	((x<<40) & 0x00FF000000000000) | \
-	((x<<24) & 0x0000FF0000000000) | \
-	((x<<8)  & 0x000000FF00000000) | \
-	((x>>8)  & 0x00000000FF000000) | \
-	((x>>24) & 0x0000000000FF0000) | \
-	((x>>40) & 0x000000000000FF00) | \
-	(x<<56) \
-)
-
-/*! @brief Load a 32 bit value from a non-aligned address. */
-#define LD_INT32(pData) ( \
-	((int32)((pData)[0])) | (((int32)((pData)[1])) << 8) | \
-	(((int32)((pData)[2])) << 16) | (((int32)((pData)[3])) << 24) \
-)
-
-/*! @brief Write a 32 bit value to a non-aligned address. */
-#define ST_INT32(pData, val) { \
-	((pData)[0] = (uint8)(val & 0x000000FF)); \
-	((pData)[1] = (uint8)((val & 0x0000FF00) >> 8)); \
-	((pData)[2] = (uint8)((val & 0x00FF0000) >> 16)); \
-	((pData)[3] = (uint8)((val & 0xFF000000) >> 24)); \
-}
-
-/*! @brief Load a 16 bit value from a non-aligned address. */
-#define LD_INT16(pData) (((int16)((pData)[0])) | (((int16)((pData)[1])) << 8))
-
-/*! @brief Write a 16 bit value to a non-aligned address. */
-#define ST_INT16(pData, val) { \
-	((pData)[0] = (uint8)(val & 0x00FF)); \
-	((pData)[1] = (uint8)((val & 0xFF00) >> 8)); \
-}
 
 /*! @brief Represents the color depth of a picture */
 enum EnOscPictureType {
@@ -241,4 +177,4 @@ OSC_ERR OscGetVersionString(char *hVersion);
 }
 #endif
 
-#endif /*OSCAR_MAIN_HEADER_FILE_WHICH_MAY_BE_INCLUDED_FROM_INSIDE_A_MODULE_H_*/
+#endif /* OSCAR_INCLUDE_OSCAR_H_ */
