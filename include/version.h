297c0c2765403baa8bbcabcd8044ef7bbd641358
Michael Schwarz <michael.schwarz@gmail.com>
2009-05-05 14:41:29 +0200

    + Beta version v1.3-RC1

--- include/version.h
+++ include/version.h
@@ -7,7 +7,7 @@
 #define OSCAR_VERSION_H_
 
 #define OSC_VERSION_MAJOR 1 /*<! @brief Major version number. (8bit)*/
-#define OSC_VERSION_MINOR 2 /*<! @brief Minor version number. (8bit)*/
-#define OSC_VERSION_PATCH 5 /*<! @brief patch number. (8bit)*/
+#define OSC_VERSION_MINOR 3 /*<! @brief Minor version number. (8bit)*/
+#define OSC_VERSION_PATCH 0 /*<! @brief patch number. (8bit)*/
 
 #endif /* OSCAR_VERSION_H_ */
