5aaf95cd4e5a3cc0739d1aabfa9795373745c5e9
Markus Berner <Markus.Berner@scs.ch>
2009-09-15 13:56:16 +0200

    + Release v2.0
    Version 2 of the Oscar framework introduces a lot of changes and new features, some of which break compatibility with existing applications.
    
    Framework:
    + Simplified framework creation and destruction
    + Totally reworked build process to be able to use the same Makefiles for all modules.
    + Added support for the black-and-white version of leanXcam
    + Easier support of additional board types
    + New macros to help with common problems
    	- Memory management in pools
    	- Object lists
    	- Error handling in TRY-CATCH blocks
    
    Module clb:
    - Has been removed for now, due to being in an unusable state.
    
    Module vis:
    + Added assembler-optimized functions for bilinear debayering to vis module. These are much faster than the previously used debayering methods.
    + Added debayering to YUV, chrominance and HSL output images
    + Added very fast debayering methods to half-size.
    + Added many more useful vision functions for:
    	- Generic 2D Filters
    	- Sobel Filtering
    	- Erosion and dilation.
    	- Binary Labeling
    
    New module jpg:
    + JPEG encoding

--- include/version.h
+++ include/version.h
@@ -24,9 +24,9 @@
  * For usage of the number fields @see OscGetRevisionNumber() 
  */
 
-#define OSC_VERSION_MAJOR 1 /*<! @brief Major version number. (8bit)*/
-#define OSC_VERSION_MINOR 3 /*<! @brief Minor version number. (8bit)*/
-#define OSC_VERSION_PATCH 1 /*<! @brief patch number. (8bit)*/
+#define OSC_VERSION_MAJOR 2 /*<! @brief Major version number. (8bit)*/
+#define OSC_VERSION_MINOR 0 /*<! @brief Minor version number. (8bit)*/
+#define OSC_VERSION_PATCH 0 /*<! @brief patch number. (8bit)*/
 #define OSC_VERSION_RC 0 /*<! @brief patch number. (8bit)*/
 
 #endif // OSCAR_INCLUDE_VERSION_H_
