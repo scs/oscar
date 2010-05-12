f84cef9c6c35173f5ce4c650b2b6f6aeb6965c95
Beat Küng <beat.kueng@scs.ch>
2010-05-12 08:44:31 +0200

    - unused typedef int64_t and uint64_t, which conflict with opencv

--- include/nih.h
+++ include/nih.h
@@ -36,10 +36,6 @@ typedef int16_t int16;
 typedef uint16_t uint16;
 typedef int32_t int32;
 typedef uint32_t uint32;
-#ifndef SKIP_INT64_TYPEDEF
-typedef int64_t int64;
-typedef uint64_t uint64;
-#endif
 typedef int OSC_ERR;
 typedef bool BOOL;
 
